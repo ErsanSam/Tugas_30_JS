725f26e10f2009e2c81b2c07f1e6f7a7728ba919
Ersan Samjaya <fernandoersan28@gmail.com>
2021-06-11 14:01:37 +0700

    Tugas 30 JS
    Tugas ini saya kumpulkan untuk NIOMIC

---
+++ index.js
@@ -0,0 +1,5 @@
+function panggilREGEXP(value){
+  var ambilData = value.match(/[se]/g);
+  console.log(ambilData);
+}
+panggilREGEXP("Para tetua adat mendiskusikan masalah kejahatan yang terjadi di rumah adat")
